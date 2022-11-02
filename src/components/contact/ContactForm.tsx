b195724d7017b2d737ec6aa66abbda4c837c5760
tboba <tymoteusz.boba@gmail.com>
2022-11-02 23:19:47 +0100

    Remove an ability to send unlimited emails

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -4,19 +4,33 @@ import * as Yup from 'yup';
 import {useState} from "react";
 
 const ContactForm = () => {
-  const errorClassNames = 'text-red-600';
+  const [isSending, setIsSending] = useState<boolean>(false);
   const [hasBeenSent, setHasBeenSent] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
+  const errorStyle = 'text-red-600';
+  const buttonIsSendingStyle = isSending ? 'opacity-50 cursor-not-allowed' : '';
+
   const handleSubmitForm = (values: FormikValues) => {
+    if (isSending) {
+      return;
+    }
+
     if (hasBeenSent) {
       setErrorMessage('Hej! Wysłałeś już do mnie wiadomość :)');
       return;
     }
 
+    setIsSending(true);
+
     emailjs.send('www-project-1', 'template_szz1ibe', values, 'e9G_Dv7sS51F-i-1T')
-      .then(() => setHasBeenSent(true))
-      .catch(() => setErrorMessage('Wiadomość nie została wysłana! Spróbuj ponownie później.'));
+      .then(() => {
+        setHasBeenSent(true);
+        setIsSending(false);
+      }).catch(() => {
+        setErrorMessage('Wiadomość nie została wysłana! Spróbuj ponownie później.');
+        setIsSending(false);
+    });
   }
 
   let FormSchema = Yup.object().shape({
@@ -48,7 +62,7 @@ const ContactForm = () => {
             <label htmlFor="name">Imię</label>
             <Field name='name' type='text' className='w-[20em] h-[2em]' />
             {errors.name && touched.name ? (
-              <p className={errorClassNames}>{errors.name}</p>
+              <p className={errorStyle}>{errors.name}</p>
             ) : null}
           </div>
 
@@ -56,7 +70,7 @@ const ContactForm = () => {
             <label htmlFor="lastName">Nazwisko</label>
             <Field name='surname' type='text' className='w-[20em] h-[2em]' />
             {errors.surname && touched.surname ? (
-              <p className={errorClassNames}>{errors.surname}</p>
+              <p className={errorStyle}>{errors.surname}</p>
             ) : null}
           </div>
 
@@ -64,7 +78,7 @@ const ContactForm = () => {
             <label htmlFor="lastName">Email</label>
             <Field name='email' type='text' className='w-[20em] h-[2em]' />
             {errors.email && touched.email ? (
-              <p className={errorClassNames}>{errors.email}</p>
+              <p className={errorStyle}>{errors.email}</p>
             ) : null}
           </div>
 
@@ -72,13 +86,19 @@ const ContactForm = () => {
             <label htmlFor="lastName">W czym mogę pomóc?</label>
             <Field as='textarea' name='description' type='text' className='w-[20em] h-[5em]' />
             {errors.description && touched.description ? (
-              <p className={errorClassNames}>{errors.description}</p>
+              <p className={errorStyle}>{errors.description}</p>
             ) : null}
           </div>
 
-          <button type="submit" className='border border-dashed bg-indigo-100 py-[0.5em] px-[1em]'>Wyślij mi maila!</button>
-          {hasBeenSent && !errorMessage && <p className={errorClassNames}>Wiadomość została wysłana :)</p>}
-          {errorMessage && <p className={errorClassNames}>{errorMessage}</p>}
+          <button
+            type="submit"
+            className={`border border-dashed bg-indigo-100 py-[0.5em] px-[1em] ${buttonIsSendingStyle}`}>
+            Wyślij mi maila!
+          </button>
+
+          {isSending && <p className={errorStyle}>Trwa wysyłanie maila...</p>}
+          {hasBeenSent && !errorMessage && <p className={errorStyle}>Wiadomość została wysłana :)</p>}
+          {errorMessage && <p className={errorStyle}>{errorMessage}</p>}
         </Form>
       )}
         </Formik>
