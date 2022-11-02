df8536ba217b5271a9d7b17d4a087f42f6884b18
tboba <tymoteusz.boba@gmail.com>
2022-11-02 20:48:24 +0100

    Add font to the footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className='bottom-0 table w-full bg-stone-500 h-[3em] text-center'>
-      <h1 className='table-cell align-middle text-white'>Tymoteusz Boba &copy; 2022</h1>
+      <h1 className='table-cell align-middle text-white font-raleway'>Tymoteusz Boba &copy; 2022</h1>
     </div>
   );
 }
